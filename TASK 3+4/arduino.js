a84d060f49c9c595f1afb161730a2bed66ae0985
Wesam Alqaadan <107884818+wesam-abdullah@users.noreply.github.com>
2022-07-29 18:38:59 +0300

    Add files via upload

---
+++ TASK 3+4/arduino.js
@@ -0,0 +1,36 @@
+let isConnectted = false;
+     let port;
+     let writer;
+     var target_id;
+     const enc = new TextEncoder();
+
+ async function onChangespeech() {
+       if (!isConnectted) {
+         alert("you must connect to the usb in order to use this.");
+         return;
+       }
+
+ try {
+     const commandlist = content;
+     const commandSplit = commandlist.split(" ")
+     const command = commandSplit.slice(-1);
+     const computerText = `${command}@`;
+     await writer.write(enc.encode(computerText));
+          }
+       catch(e){
+         console.log(e);
+       } }
+
+  async function onConnectUsb() {
+     try {
+       const requestOptions = {
+         filters: [{ usbVendorId: 0x2341 }],
+       };
+
+   port = await navigator.serial.requestPort(requestOptions);
+   await port.open({ baudRate: 115200 });
+   writer = port.writable.getWriter();
+   isConnectted = true; }
+     catch(e){
+       console.log("err", e);
+     } }
